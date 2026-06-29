eb2324a6b3d7450c251afec65960caa4803b6a3f
raajmajumdar <106169615+raajmajumdar@users.noreply.github.com>
2026-06-29 15:35:52 +0530

    all
    css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -192,14 +192,16 @@
 	</div>
 
 
-
-
-
 </div>
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
+
+<!-- collapsed / Sidebar -->
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<!--
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+   -->
 <script>
   // Sidebar toggle
   const toggleSidebar = document.getElementById('toggleSidebar');
