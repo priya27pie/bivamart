3baa3560ebbe12a288a9cad7ec0b84e1e06b83d7
raajmajumdar <106169615+raajmajumdar@users.noreply.github.com>
2026-07-02 15:40:42 +0530

    ok

--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -2,12 +2,112 @@
 @section('middle')
 
 <div class="inner-profile">
-  <img src="{{asset('images/banner2.jpg')}}" alt="" class="inner-banner-img">
-  <p  data-aos="zoom-in" style="transition:all 1500ms ease-in-out;">Edit_profile</p>
+  <img src="{{asset('images/profile-banner.png')}}" alt="" class="inner-banner-img">
+  <p  data-aos="zoom-in" style="transition:all 1500ms ease-in-out;">Wish  <i class="fa-regular fa-heart"></i>  list</p>
    <div class="particle-network-animation"></div>
 </div>
 
 
 
+<!--  New Arrival   -->
+<div class="Top-Trending Best-Sellers New-Arrival Wish-block" style="">
+    <div class="title-home" data-aos="fade-down" style="transition:all 1100ms ease-in-out;">
+        <h2 style="">Wish  <i class="fa-regular fa-heart"></i>  list <span style="">bivamart.com</span></h2>
+    </div> 
+    <div class="container">
+        <div class="row">
+			<div class="col-md-2 col-sm-4 col-xs-12">
+                <div class="trending-box">
+                    <div class="trending-img">
+                        <img src="{{asset('images/Trending1.png')}}" alt="" class="" />
+                        <h6>₹ 15% OFF</h6>
+                        <i class="fa-solid fa-heart"></i>
+                    </div>
+                    <h3>TIN TIRRIKE BHOI</h3>
+                    <h4><b>WRITER :</b> Trijit Kar </h4>
+                    <h5><b>₹ </b> 499/- <del>599</del></h5>
+                    <a href="#">
+                        <i class="fa fa-bag-shopping"></i> Add to Bag
+                    </a>
+                </div> 
+            </div> 
+			<div class="col-md-2 col-sm-4 col-xs-12">
+                <div class="trending-box">
+                    <div class="trending-img">
+                        <img src="{{asset('images/Pre-Order2.png')}}" alt="" class="" />
+                        <h6>₹ 15% OFF</h6>
+                        <i class="fa-solid fa-heart"></i>
+                    </div>
+                    <h3>TIN TIRRIKE BHOI</h3>
+                    <h4><b>WRITER :</b> Trijit Kar </h4>
+                    <h5><b>₹ </b> 499/- <del>599</del></h5>
+                    <a href="#">
+                        <i class="fa fa-bag-shopping"></i> Add to Bag
+                    </a>
+                </div> 
+            </div>            
+			<div class="col-md-2 col-sm-4 col-xs-12">
+                <div class="trending-box">
+                    <div class="trending-img">
+                        <img src="{{asset('images/Trending2.png')}}" alt="" class="" />
+                        <h6>₹ 15% OFF</h6>
+                        <i class="fa-solid fa-heart"></i>
+                    </div>
+                    <h3>TIN TIRRIKE BHOI</h3>
+                    <h4><b>WRITER :</b> Trijit Kar </h4>
+                    <h5><b>₹ </b> 499/- <del>599</del></h5>
+                    <a href="#">
+                        <i class="fa fa-bag-shopping"></i> Add to Bag
+                    </a>
+                </div> 
+            </div> 
+			<div class="col-md-2 col-sm-4 col-xs-12">
+                <div class="trending-box">
+                    <div class="trending-img">
+                        <img src="{{asset('images/Trending3.png')}}" alt="" class="" />
+                        <h6>₹ 15% OFF</h6>
+                        <i class="fa-solid fa-heart"></i>
+                    </div>
+                    <h3>TIN TIRRIKE BHOI</h3>
+                    <h4><b>WRITER :</b> Trijit Kar </h4>
+                    <h5><b>₹ </b> 499/- <del>599</del></h5>
+                    <a href="#">
+                        <i class="fa fa-bag-shopping"></i> Add to Bag
+                    </a>
+                </div> 
+            </div>                       
+			<div class="col-md-2 col-sm-4 col-xs-12">
+                <div class="trending-box">
+                    <div class="trending-img">
+                        <img src="{{asset('images/Trending4.png')}}" alt="" class="" />
+                        <h6>₹ 15% OFF</h6>
+                        <i class="fa-solid fa-heart"></i>
+                    </div>
+                    <h3>TIN TIRRIKE BHOI</h3>
+                    <h4><b>WRITER :</b> Trijit Kar </h4>
+                    <h5><b>₹ </b> 499/- <del>599</del></h5>
+                    <a href="#">
+                        <i class="fa fa-bag-shopping"></i> Add to Bag
+                    </a>
+                </div> 
+            </div>  
+			<div class="col-md-2 col-sm-4 col-xs-12">
+                <div class="trending-box">
+                    <div class="trending-img">
+                        <img src="{{asset('images/Pre-Order3.png')}}" alt="" class="" />
+                        <h6>₹ 15% OFF</h6>
+                        <i class="fa-solid fa-heart"></i>
+                    </div>
+                    <h3>TIN TIRRIKE BHOI</h3>
+                    <h4><b>WRITER :</b> Trijit Kar </h4>
+                    <h5><b>₹ </b> 499/- <del>599</del></h5>
+                    <a href="#">
+                        <i class="fa fa-bag-shopping"></i> Add to Bag
+                    </a>
+                </div> 
+            </div>                  
+        </div>
+    </div>     
+</div>
 
 @endsection
