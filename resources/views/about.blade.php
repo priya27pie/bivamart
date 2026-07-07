56dee3c44f805337f25aacb82ed49520fd41070d
raajmajumdar <106169615+raajmajumdar@users.noreply.github.com>
2026-07-07 14:53:06 +0530

    page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,140 @@
+<?php 
+    include("header.php");
+?>
+<!-- // Header include -->
+
+
+<div class="inner-banner">
+  <img src="images/iner-banner.png" alt="" class="inner-banner-img">
+  <p  data-aos="zoom-in" style="transition:all 1500ms ease-in-out;">About us</p>
+   <div class="particle-network-animation"></div>
+</div>
+
+
+
+<div class="about-inner aos-init aos-animate" data-aos="fade-down" style="transition:all 1500ms ease-in-out;">
+    <!-- tittle heading -->
+    <div class="title-home" data-aos="fade-down" style="transition:all 1300ms ease-in-out;">
+        <h2>About us<span> bivamart.com</span></h2>
+    </div> 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-sx-12">
+                <div class="left-text-top aos-init aos-animate" data-aos="fade-down" style="padding:0px 0; transition:all 1400ms ease-in-out;">
+                    <h2>At Bivamart, we enjoy curating special events across our stores.</h2>
+                    <p>bivamart.com Bookstores was founded in <b>( demo text all )</b> with a simple yet passionate mission – To positively impact the world through the power of reading and learning.<br>Right from our first store in Mumbai to the 120 stores across 40 cities today, we have continued to serve and nurture our community of readers for over 3 decades. As India's leading bookstore retailer, we champion books and nourish a love for the written word through a rich, handpicked collection covering numerous topics. <br>Our stores are thoughtfully designed with interiors that inspire and relax, allowing quiet spaces to help you discover great books.</p>
+                    <h2> Mission and Vision </h2>
+                    <p>In addition to our diverse selection of books, we have premium stationery and toys, to make your life a little easier, and a lot more colourful!</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-sx-12">
+                <div class="about-inner-item aos-init aos-animate" data-aos="zoom-in" style="transition: all 1300ms ease-in-out;">
+                    <img src="images/about-iner-bg.png" alt="" class="" style="width:100%;border-radius: 0px;">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+
+<!--  Best Sellers  -->
+<div class="Top-Trending Best-Sellers">
+    <div class="title-home" data-aos="fade-down" style="transition:all 1100ms ease-in-out;">
+        <h2>Best Sellers  <span>bivamart.com</span></h2>
+    </div> 
+    <div class="container">
+        <div id="Best-Sellers" class="owl-carousel">
+            <div class="item">
+                <div class="trending-box">
+                    <div class="trending-img">
+                        <img src="images/Trending1.png" alt="" class="" />
+                        <h6>₹ 15% OFF</h6>
+                    </div>
+                    <h3>TIN TIRRIKE BHOI</h3>
+                    <h4><b>WRITER :</b> Trijit Kar </h4>
+                    <h5><b>₹ </b> 499/- <del>599</del></h5>
+                    <a href="single.php">
+                        <i class="fa fa-bag-shopping"></i> Add to Bag
+                    </a>
+                </div> 
+            </div>   
+            <div class="item">
+                <div class="trending-box">
+                    <div class="trending-img">
+                        <img src="images/Trending3.png" alt="" class="" />
+                        <h6>₹ 15% OFF</h6>
+                    </div>
+                    <h3>Podipisir Bormi Baksho</h3>
+                    <h4><b>WRITER :</b> Saikat Mukhopadhyay </h4>
+                    <h5><b>₹ </b> 499/- <del>599</del></h5>
+                    <a href="single.php">
+                        <i class="fa fa-bag-shopping"></i> Add to Bag
+                    </a>
+                </div> 
+            </div> 
+            <div class="item">
+                <div class="trending-box">
+                    <div class="trending-img">
+                        <img src="images/Pre-Order2.png" alt="" class="" />
+                        <h6>₹ 15% OFF</h6>
+                    </div>
+                    <h3>Kauriburi Temple</h3>
+                    <h4><b>WRITER :</b> Avik Sarkar </h4>
+                    <h5><b>₹ </b> 499/- <del>599</del></h5>
+                    <a href="single.php">
+                        <i class="fa fa-bag-shopping"></i> Add to Bag
+                    </a>
+                </div> 
+            </div> 
+            <div class="item">
+                <div class="trending-box">
+                    <div class="trending-img">
+                        <img src="images/Trending4.png" alt="" class="" />
+                        <h6>₹ 15% OFF</h6>
+                    </div>
+                    <h3>Nastik Panditer Bhita</h3>
+                    <h4><b>WRITER :</b> A.Dipankar </h4>
+                    <h5><b>₹ </b> 499/- <del>599</del></h5>
+                    <a href="single.php">
+                        <i class="fa fa-bag-shopping"></i> Add to Bag
+                    </a>
+                </div> 
+            </div>
+
+        </div>
+    </div>     
+</div>
+<!-- //  Best Sellers -->
+
+
+
+<!-- Shop by Publishers -->
+<div class="Publishers-Shop">
+    <div class="title-home" data-aos="fade-down" style="transition:all 1300ms ease-in-out;">
+        <h2>Shop by Publishers<span>bivamart.com</span></h2>
+    </div> 
+    <div class="container">
+        <ul class="Publishers-box"  data-aos="fade-down" style="transition:all 1400ms ease-in-out;" >
+            <li><a href="#">Aakar Books <span>Delhi</span></a></li>
+            <li><a href="#">ABD Pub. <span>Jaipur</span></a></li> 
+            <li><a href="#">Royal Book Company  <span>Lucknow</span></a></li>
+            <li><a href="#">Natraj Pub <span>Dehradun</span></a></li>             
+            <li><a href="#">Wolters Kluwer <span>India (CCH)</span></a></li>
+            <li><a href="#">Ananda Publishers. <span>West Bengal</span></a></li> 
+            <li><a href="#">Patra Bharati <span>West Bengal</span></a></li>
+            <li><a href="#">Tulsi Prakashani <span>West Bengal</span></a></li> 
+        </ul>       
+    </div>     
+</div>      
+<!--//  Shop by Publishers-->
+
+
+
+<!-- Header include -->
+<?php 
+    include("footer.php");
+?>
+<!-- // Header include -->
---
+++ resources/views/faq.blade.php
@@ -0,0 +1,88 @@
+<?php 
+    include("header.php");
+?>
+<!-- // Header include -->
+
+
+<div class="inner-banner">
+  <img src="images/iner-banner.png" alt="" class="inner-banner-img">
+  <p  data-aos="zoom-in" style="transition:all 1500ms ease-in-out;">Faq</p>
+   <div class="particle-network-animation"></div>
+  <div class="container">
+    <!--<ul>-->
+    <!--  <li><a href="faq.php" class="active">Faq</a></li> |-->
+    <!--  <li><a href="index.php" class="">Home</a></li>    -->
+    <!--</ul>-->
+  </div>
+</div>
+
+
+
+<div class="terms faq" data-aos="fade-down" style="transition:all 1500ms ease-in-out;">
+  	<div class="container">
+    <div class="title-home" data-aos="fade-down" style="transition:all 1300ms ease-in-out;">
+        <h2>Frequently asked Questions<span> bivamart.com</span></h2>
+    </div> 
+
+  		<div class="row">
+  			<div class="col-md-12 col-sm-12 col-xm-12">
+
+            <div class="accordion-box">
+                <button class="accordion"><b>Q.</b> Is this Publishing Process legal? </button>
+                <div class="panel" style="display: none;">
+                    <p><b>Demo text </b> All verification activities are conducted legally and ethically.</p>
+                </div>
+            </div>
+            <div class="accordion-box">
+                <button class="accordion"><b>Q.</b> Will the subject know about the Publishing Process? </button>
+                <div class="panel">
+                    <p><b>Demo text </b> All verification activities are conducted legally and ethically.</p>
+                </div>
+            </div>
+            <div class="accordion-box">
+                <button class="accordion"><b>Q.</b> How long does verification Publishing Process?</button>
+                <div class="panel">
+                    <p><b>Demo text </b> All verification activities are conducted legally and ethically.</p>
+                </div>
+            </div>
+            <div class="accordion-box">
+                <button class="accordion"><b>Q.</b> Do you Publishing Process results? </button>
+                <div class="panel">
+                    <p><b>Demo text </b> All verification activities are conducted legally and ethically.</p>
+                </div>
+            </div>
+
+
+  			</div>
+  		</div>
+  	</div>
+</div>
+
+
+
+<!-- Shop by Publishers -->
+<div class="Publishers-Shop">
+    <div class="title-home" data-aos="fade-down" style="transition:all 1300ms ease-in-out;">
+        <h2>Shop by Publishers<span>bivamart.com</span></h2>
+    </div> 
+    <div class="container">
+        <ul class="Publishers-box"  data-aos="fade-down" style="transition:all 1400ms ease-in-out;" >
+            <li><a href="#">Aakar Books <span>Delhi</span></a></li>
+            <li><a href="#">ABD Pub. <span>Jaipur</span></a></li> 
+            <li><a href="#">Royal Book Company  <span>Lucknow</span></a></li>
+            <li><a href="#">Natraj Pub <span>Dehradun</span></a></li>             
+            <li><a href="#">Wolters Kluwer <span>India (CCH)</span></a></li>
+            <li><a href="#">Ananda Publishers. <span>West Bengal</span></a></li> 
+            <li><a href="#">Patra Bharati <span>West Bengal</span></a></li>
+            <li><a href="#">Tulsi Prakashani <span>West Bengal</span></a></li> 
+        </ul>       
+    </div>     
+</div>      
+<!--//  Shop by Publishers-->
+
+
+<!-- Header include -->
+<?php 
+    include("footer.php");
+?>
+<!-- // Header include -->
